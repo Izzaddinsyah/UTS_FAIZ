41072b2686bc0e66e0944ec81b9ffc8d6ff67951
Faiz <faizrachmanto@gmail.com>
2024-10-25 20:05:22 +0700

    HEY

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.google.gms.google.services)
+    kotlin("kapt")
 }
 
 android {
@@ -40,6 +41,13 @@ android {
 }
 
 dependencies {
+    implementation("com.google.firebase:firebase-database-ktx:20.3.0")
+    implementation("com.google.firebase:firebase-storage-ktx:20.3.0")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    implementation(libs.androidx.annotation)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    kapt("com.github.bumptech.glide:compiler:4.12.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
---
+++ app/src/main/java/com/example/uts_map_lab/HistoryData.kt
@@ -0,0 +1,6 @@
+package com.example.uts_map_lab
+
+data class HistoryData(
+    val imageUrl: String? = null,
+    val uploadTime: Long? = null
+)
